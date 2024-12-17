017d6a70503c0288aeab36ed97f0683ea0910fd1
gwitus <emanuelbarbosati@gmail.com>
2024-12-17 07:55:33 +0000

    subindo js para o carrossel

---
+++ webAppMilbr/wwwroot/js/carrossel.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let time = 5000, // Intervalo de tempo (5 segundos)
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#index-area-cliente-conteudo-carrossel img"),
+    max = images.length;
+
+    function nextImage() {
+        images[currentImageIndex].classList.remove("selected");
+        currentImageIndex++;
+        if (currentImageIndex >= max) currentImageIndex = 0;
+
+        images[currentImageIndex].classList.add("selected");
+    }
+
+    function start() {
+        setInterval(() => {
+            nextImage();
+        }, time);
+    }
+    window.addEventListener("load", start);
+});
