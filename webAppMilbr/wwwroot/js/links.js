ef81792cbca089be51d73dfdb82f35d661b1da56
gwitus <emanuelbarbosati@gmail.com>
2024-12-22 09:03:42 +0000

    melhorias na navegação 1.0

---
+++ webAppMilbr/wwwroot/js/links.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); // Impede o comportamento padrão do link
+
+        const targetId = this.getAttribute('href').slice(1); // Obtém o ID sem o '#'
+        const targetElement = document.getElementById(targetId); // Seleciona o elemento alvo
+
+        if (targetElement) {
+            // Rola suavemente até o elemento alvo
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
